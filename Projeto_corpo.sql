7ece7ff0782c10036e864382f25562cbe023b962
TomasPeres <fc63721@alunos.fc.ul.pt>
2025-12-07 15:46:39 +0000

    regista_utilizador

--- Projeto_corpo.sql
+++ Projeto_corpo.sql
@@ -17,7 +17,7 @@ num_albuns NUMBER;
         INSERT INTO artista (isni, nome, inicio) VALUES (isni_in, nome_in, inicio_in);
     END regista_artista;
 
-    --método 2
+
     PROCEDURE regista_album(
         ean_in IN album.ean%TYPE, titulo_in IN album.titulo%TYPE, tipo_in IN album.tipo%TYPE, ano_in IN album.ano%TYPE, 
         artista_in IN album.artista%TYPE, suporte_in IN album.suporte%TYPE, versao_in IN album.versao%TYPE := NULL)
@@ -27,4 +27,13 @@ num_albuns NUMBER;
         VALUES (ean_in, titulo_in, tipo_in, ano_in, artista_in, suporte_in, versao_in);
     END regista_album;
 
+
+    PROCEDURE regista_utilizador(username_in IN utilizador.username%TYPE, email_in IN utilizador.email%TYPE, 
+    senha_in IN utilizador.senha%TYPE, nascimento_in IN utilizador.nascimento%TYPE, 
+    artista_in IN utilizador.artista%TYPE := NULL)
+    IS
+    BEGIN
+        INSERT INTO utilizador(username, email, senha, nascimento, artista)
+        VALUES (username_in, email_in, senha_in, nascimento_in, artista_in)
+    END regista_utilizador
 
